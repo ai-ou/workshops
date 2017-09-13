c4c598f4a304bdafdefa2f52d45f00791dafd6a3
gawhaley <alexw999@gmail.com>
2017-09-13 19:55:39 -0400

    basic numpy tutorial WIP

---
+++ numpy.py
@@ -0,0 +1,20 @@
+import numpy as np
+import math
+
+'''
+ Use math.exp() to get y = e^x
+'''
+
+x = 3
+y = math.exp(x)
+print("x = %d, e^x = %d") % (x,y)
+
+x = 0
+y = math.exp(x)
+print("x = %d, e^x = %d") % (x,y)
+
+'''
+Note that if
+x = [1,2,3]
+then math.exp(x) crashes. Feel free to try this.
+'''
