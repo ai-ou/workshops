9aec7d4c9718d412313575b8d393688c39f0b093
Lukas Palmer <lukas@lnpalmer.io>
2017-09-13 20:06:10 -0400

    example commit

--- git-going/numpy.py
+++ git-going/numpy.py
@@ -1,10 +1,7 @@
 import numpy as np
 import math
 
-'''
- Use math.exp() to get y = e^x
-'''
-
+# Use math.exp() to get y = e^x
 x = 3
 y = math.exp(x)
 print("x = %d, e^x = %d") % (x,y)
@@ -18,3 +15,14 @@ Note that if
 x = [1,2,3]
 then math.exp(x) crashes. Feel free to try this.
 '''
+
+# matrix multiplication
+A = np.array([[1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9]])
+
+B = np.array([[1, 2, 0],
+              [2, 4, 2],
+              [0, 2, 1]])
+
+AB = np.matmul(A, B)
